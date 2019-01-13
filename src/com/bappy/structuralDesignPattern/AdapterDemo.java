74b878547ca81cdc4af593befcf08315fcf29e7c
masudbappy <masud.bappy90@gmail.com>
2019-01-13 23:49:13 +0600

    add adapter pattern

---
+++ src/com/bappy/structuralDesignPattern/AdapterDemo.java
@@ -0,0 +1,12 @@
+package com.bappy.structuralDesignPattern;
+
+import java.util.List;
+
+public class AdapterDemo {
+    public static void main(String[] args) {
+
+        EmployeeClient client = new EmployeeClient();
+        List<Employee> employees = client.getEmployees();
+        System.out.println(employees);
+    }
+}
---
+++ src/com/bappy/structuralDesignPattern/EmployeeClient.java
@@ -0,0 +1,21 @@
+package com.bappy.structuralDesignPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClient {
+    public List<Employee> getEmployees() {
+
+        List<Employee> employees = new ArrayList<>();
+        Employee employeeFromDB = new EmployeeDB("123", "jhon", "whick", "jhon@gamil.com");
+        employees.add(employeeFromDB);
+
+        EmployeeLdap employeeFromLdap = new EmployeeLdap("333","masud","bappy","bappy@gmail.com");
+        employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+
+        EmployeeCSV employeeFromCSV = new EmployeeCSV("12345,sherlock,homes,sherloc@gmail.com");
+        employees.add(new EmployeeAdapterCSV(employeeFromCSV));
+
+        return employees;
+    }
+}
